f8031016a3bdf1229787e0dc86ecd0cebe820e33
Shezan Mahmud <amishezanmahmud@gmail.com>
2021-10-20 02:33:32 +0600

    up
     django-import-export

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views import generic
 from .models import *
 from django.urls import reverse
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 
@@ -16,3 +17,8 @@ def index(request):
     }
   
     return render(request, 'index.html', context)
+
+    
+class search(TemplateView):
+   template_name = 'search.html'
+   
