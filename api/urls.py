8cdef5f581a389f0b7fa4863de2b19b36027c0fd
KhaledMasud Babu <31078651+imkhaled404@users.noreply.github.com>
2021-10-20 02:17:32 +0600

    UI convert done in py

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,22 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.c_first, name='c_first'),
+    path('', views.c_second, name='c_second'),
+    path('', views.c_third, name='c_third'),
+    path('', views.c_fourth, name='c_fourth'),
+    path('', views.committees, name='committees'),
+    path('', views.confirmation, name='confirmation'),
+    path('', views.contact, name='contact'),
+    path('', views.edit, name='edit'),
+    path('', views.eligible, name='eligible'),
+    path('', views.gallery, name='gallery'),
+    path('', views.goldmadelists, name='goldmadelists'),
+    path('', views.payment, name='payment'),
+    path('', views.registration, name='registration'),
+    path('', views.rules, name='rules'),
+    path('', views.schedule, name='schedule'),
+    path('', views.speech_detail, name='speech_detail'),
+    path('', views.speech, name='speech'),
+    path('', views.venu, name='venu'),
 ]
--- api/views.py
+++ api/views.py
@@ -8,11 +8,168 @@ from django.urls import reverse
 
 
 def index(request):
-   
-    posts = "Shezan Mahmud"
-    
+    posts = "Index"
     context = {
        'posts': posts
     }
   
     return render(request, 'index.html', context)
+
+
+def c_first(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'c_first.html', context)
+
+
+def c_second(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'c_second.html', context)
+
+
+def c_third(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'c_third.html', context)
+
+
+def c_fourth(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'c_fourth.html', context)
+
+
+def committees(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'committees.html', context)
+
+
+def confirmation(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'confirmation.html', context)
+
+
+def contact(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'contact.html', context)
+
+
+def edit(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'edit.html', context)
+
+
+def eligible(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'eligible.html', context)
+
+
+def gallery(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'gallery.html', context)
+
+def goldmadelists(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'goldmadelists.html', context)
+
+
+def payment(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'payment.html', context)
+
+
+
+def registration(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'registration.html', context)
+
+
+def rules(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'rules.html', context)
+
+def schedule(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'schedule.html', context)
+
+
+def speech_detail(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'speech_detail.html', context)
+
+def speech(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'speech.html', context)
+
+def venu(request):
+    posts = "Index"
+    context = {
+       'posts': posts
+    }
+  
+    return render(request, 'venu.html', context)
