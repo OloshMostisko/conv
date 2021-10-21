e46a0fbee5ce0731de77afe11cb674b59b36d48e
Shezan Mahmud <shezan@constantmd.com>
2021-10-21 13:11:39 +0600

    up

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Student(models.Model):
     intake = models.IntegerField( verbose_name = 'Intake', blank=False, null=False)
     std_full_name = models.CharField( verbose_name = 'Name', max_length=100, null=False, blank=True, default="")
     Cell_Phone = models.IntegerField( verbose_name = 'Phone', null=False, blank=True, default=0)
-    emai = models.EmailField( verbose_name = 'Email', null = False, blank=True, default= "")
+    email = models.EmailField( verbose_name = 'Email', null = False, blank=True, default= "")
 
     totalMejor = models.IntegerField( verbose_name = 'Total Major',  null=False, default=0, blank=True)
     paidFor = models.IntegerField( verbose_name = 'Paid For Major',  null=False, default=0, blank=True)
@@ -23,3 +23,29 @@ class Student(models.Model):
 
     def __str__(self):
         return self.std_full_name 
+
+
+class Registration(models.Model):
+    p_usename = models.CharField(verbose_name = 'Degree', max_length=50,null = False, blank=False)
+    s_id = models.IntegerField(verbose_name = 'Student ID',  unique = True, null = False, blank = False)
+    intake = models.IntegerField( verbose_name = 'Intake', blank=False, null=False)
+    section = models.IntegerField( verbose_name = 'Section', blank=False, null=False)
+
+    std_full_name = models.CharField( verbose_name = 'Name', max_length=100, null=False, blank=True, default="")
+    Cell_Phone = models.IntegerField( verbose_name = 'Phone', null=False, blank=True, default=0)
+    email = models.EmailField( verbose_name = 'Email', null = False, blank=True, default= "")
+
+    totalMejor = models.IntegerField( verbose_name = 'Total Major',  null=False, default=0, blank=True)
+    paidFor = models.IntegerField( verbose_name = 'Paid For Major',  null=False, default=0, blank=True)
+
+   
+    hasPaid = models.BooleanField( verbose_name = 'Payment Done',null=False, default=False)
+    regDate = models.DateField(verbose_name = 'Registration Date', auto_now = True)
+
+    def __str__(self):
+        return self.std_full_name
+    class Meta:
+        verbose_name_plural = "Registred Student"
+
+    
+    
