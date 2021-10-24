532cc93f2a006119511deee53b119dc56eccfa03
KhaledMasud Babu <31078651+imkhaled404@users.noreply.github.com>
2021-10-25 03:04:37 +0600

    mail

--- backend/settings.py
+++ backend/settings.py
@@ -152,5 +152,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS =True
-# EMAIL_HOST_USER = ''
-# EMAIL_HOST_PASSWORD = ''
+# EMAIL_HOST_USER = 'convocation@bubt.edu.bd'
+# EMAIL_HOST_PASSWORD = 'convo@bubt$123$'
