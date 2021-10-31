2cee116143758398cfc3e23d5682f4a7e4bda9f5
Shezan Mahmud <amishezanmahmud@gmail.com>
2021-10-31 16:43:31 +0600

    Revert "update"
    This reverts commit 1a1870a3b2fe080a8a2452147313b69f1768372c.

--- backend/settings.py
+++ backend/settings.py
@@ -131,12 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 # MEDIA
